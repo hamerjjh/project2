192cd93688f81225caa6e91be4446af6de02bfb9
hamerjjh <hamerjjh2@gmail.com$ git config --get user.namegit config --get user.namegit config --global user.email hamerjjh2@gmail.com>
2017-09-28 18:06:54 -0400

    creation of brand controller

---
+++ routes/brandController.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const Schema = require("../db/schema.js");
+const BrandModel = Schema.BrandModel;
+/* Index Route */
+router.get('/', (req, res) => {
+    BrandModel.find({})
+    .then((brands) => {
+        res.render('brands/index', {
+            brands: brands
+        })
+    })
+})
+
+module.exports = router
